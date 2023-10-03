2dc121902a5c62fc124803d2107a8e494791e395
abhisharma8860855650 <146344486+abhisharma8860855650@users.noreply.github.com>
2023-10-03 20:00:59 +0530

    about us file

---
+++ about.php
@@ -0,0 +1,314 @@
+<?php include('includes/header.php')?>
+     <!--Breadcrumb section Start-->
+     <Section class="breadcrumb ">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_9">
+                    <h2>History of GIP</h2>
+                </div>
+                <div class="col_3 breadcrumb_lnk">
+                    <ul>
+                        <li>Home</li>
+                        <li>About GIP</li>
+                        <li>History of GIP</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <!--Breadcrumb section End-->
+    <!--History GIP Section Start-->
+    <section class="history_gip_section  gradient_container_topbar">
+        <div class="flex_row top_discription_box"></div>
+    <div class="container_fluid ">
+                
+        <div class="flex_row">
+            <div class="col_12">
+                <p>The Ministerial Communiqué of the G20 Environmental Ministers Meet during the Saudi Arabia Summit 2020 recognised the urgent
+                    need for bold, coordinated, and collective initiatives on land protection and restoration and on sustainable land 
+                    management and use to strengthen existing efforts, such as the Bonn Challenge, the United Nations Decade on Ecosystem 
+                    Restoration, and other multilateral initiatives, as well as to promote new ones. One of the key elements of the initiative 
+                    was to establish a land conservation, sustainable management, and restoration website. The Saudi Summit declared to support
+                    the launch of a Global Initiative on Reducing Land Degradation and Enhancing Conservation of Terrestrial Habitats which
+                    aims to support existing efforts to prevent, halt, and reverse land degradation and habitat loss through sharing of 
+                    knowledge and best practices on protecting, conserving, sustainably managing, restoring, and rehabilitating degraded 
+                    land, and by showcasing and disseminating publicly available data and information on degraded lands and conservation/
+                    restoration efforts – Global Restoration Information Hub (GRIH).
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+    <!--History GIP Section Start-->
+    <!--Minister Images Section Start-->
+    <section class="minister_images_section">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_6">
+                    <img src="assets/images/aboutpage/g20leaders.jpg" class="minister_images_g20">
+                </div>
+                <div class="col_6">
+                    <img src="assets/images/aboutpage/g20leadersmeeting.jpg" class="minister_images_g20">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Minister Images Section Start-->
+    <!--Description Section Start-->
+    <section class="minister_images_descriptipon">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_12">
+                    <p>
+                        The G20 Environment communiqué held in Italy 2021 summit stressed that biodiversity conservation, protection, 
+                        sustainable use, and restoration are critical to achieve the SDGs and to halt and reverse biodiversity loss.
+                    </p>
+                    <p>
+                        In 2022 the G20 Indonesia Summit pledged to continue to support the G20 Global Initiative on Reducing Land 
+                        Degradation and Enhancing Conservation of Terrestrial Habitats adopted under Saudi Arabia’s Presidency and to 
+                        enhance land protection, conservation, and restoration, address common land tenure challenges in accordance with 
+                        national legislation in line with the recent decision 27 at UNCCD COP15 and promote inclusive sustainable land
+                        management by actively contributing to the operational development of the Global Initiative.
+                    </p>
+                    <p> 
+                        In 2023, the India’s presidency has identified Arresting Land Degradation, Accelerating Ecosystem Restoration and 
+                        Enriching Biodiversity as one of the key priorities for deliberation as a part of the Environment and Climate 
+                        Sustainability Working Group (ECSWG). India proposes a knowledge sharing platform- Gandhinagar Information Portal 
+                        (GIP) enhance the Global Restoration Information Website being developed under the G20 Global Land Initiative.
+                    </p>
+                    <p><button class="minister_box_button">List of Documents</button></p>
+                </d iv>
+            </div>
+        </div>
+    </section>
+    <!--Description Section Start-->
+    <!--GIP Section Start-->
+    <section class="gip_section   ">
+        <div class="container_fluid">
+            <div class="container members_section">
+                <div class="gradient_container">
+                <div class="flex_row">
+                    <div class="col_12">
+                        <h5>Gandhinagar Information Platform (GIP)</h5>
+                        <p>The Gandhinagar Implementation Roadmap (GIR) and the Gandhinagar Information Platform (GIP) are centred around 
+                            the restoration of landscapes of forest fire impacted areas and mining affected areas, in order to reinforce 
+                            the G20 Global Land Initiative (GLI) implementation. This does not preclude global action across other 
+                            ecosystems based on the Kunming Montreal GBF and other relevant initiatives and frameworks such as the 2030
+                            Agenda for Sustainable Development and its SDGs, the Paris Agreement, the UNCCD Strategic Framework, the UN 
+                            Decade on Ecosystem Restoration 2021-2030, as well as national priorities.
+                        </p>
+                        <h5>Objective and Ambition of the Gandhinagar Implementation Roadmap</h5>
+                        <p>The roadmap seeks to enhance, on a voluntary basis, mutual collaboration among participating countries to 
+                            accelerate ecological/ecosystem restoration of the identified landscapes. The initiative has 2 key objectives:
+                        </p>
+                    </div>
+                </div>
+                <div class="flex_row objective_boxes">
+                    <div class="col gip_box_first">
+                        <p>Enhance focus on ecological/ecosystem restoration of land by reinforcing the capacity of habitats to sustain
+                             the diversity of their species in particular the keystone/ indicator/ umbrella species, in accordance with
+                              the respective priorities as determined by scientific evidence, and
+                        </p>
+                    </div>
+                    <div class="col gip_box_second">
+                        <p>Enhance focus on ecological/ecosystem restoration of land by reinforcing the capacity of habitats to sustain
+                             the diversity of their species in particular the keystone/ indicator/ umbrella species, in accordance with
+                              the respective priorities as determined by scientific evidence, and
+                        </p>
+                    </div>
+                </div>
+                <div class="flex_row">
+                    <div class="col_12 boxes_discrptn">
+                        <p>Under this roadmap the participating countries on a voluntary basis shall share progress, knowledge, and best practices
+                            to enhance ecological/ecosystem restoration of landscapes through the Gandhinagar Information Platform.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex_row">
+                    <div class="col_12">
+                            <h5>Participating countries, on a voluntary basis resolve to:</h5>
+                        <p>
+                            a) Bring all forest fire degraded lands under effective restoration by 2030 (over a 2011-2020 reference period as 
+                            envisaged in CBD COP Decision 15/5) and enable their ecological/ecosystem restoration by 2040, on a voluntary basis.
+                        </p>
+                        <p>
+                            b) Bring all mining-impacted land areas under effective restoration by 2030 (over a 2011-2020 reference period as
+                            envisaged in CBD COP Decision 15/5) as per country’s plan and circumstances and enable their ecological/ecosystem 
+                            restoration by 2040, on a voluntary basis.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            </div>   
+        </div>
+    </section>
+    <!--GIP Section Start-->
+   
+    <!--Pillars Section Start-->
+    <section class="pilrs_section common_spacing">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <h5>Pillars of the Gandhinagar Implementation Roadmap</h5>
+                <p>All the participating countries to the roadmap are invited to nominate national nodal institutes/experts/
+                    organizations engaged in the restoration of the two identified landscapes on a voluntary basis. These nodal 
+                    institutions/experts/organizations will form the backbone of the GIR and help achieve its objectives by acting 
+                    across the below mentioned 5 pillars.</p>
+            </div>
+            <div class="flex_row">
+                <div class="col gradient_container_shwcase">
+                    <div class="gradient_container">
+                   <h5 class="shwcase"> SHOWCASE</h5>
+                    <P>progress on ecological/ecosystem restoration of the identified landscapes by compiling annual reports on the 
+                        types of projects and best practices of conservation</P>
+                </div>
+            </div>
+                <div class="col gradient_container_shwcase">
+                    <div class="gradient_container_blue">
+                        <h5 class="engage">ENGAGE</h5>
+                        <P>national nodal institutes/experts/organizations for scientific research, knowledge and technology sharing as well as in supporting monitoring...
+                            <br> <a href="">[View More]</a>
+                        </P>
+                    </div>
+                </div>
+                <div class="col gradient_container_shwcase">
+                    <div class="gradient_container_green">
+                        <h5 class="colbarate">COLLABORATE</h5>
+                            <P>through the network of nodal institutions/experts/organizations for capacity building and knowledge sharing, including traditional knowledge...
+                                <br> <a href="">[View More]</a>
+                            </P>
+                       </div> 
+                </div>
+                <div class="col gradient_container_shwcase">
+                    <div class="gradient_container_lightblue">
+                        <h5 class="suport"> SUPPORT</h5>
+                        <P>especially developing and least developed countries in accelerating ecological/ecosystem restoration by mobilizing sufficient, adequate and timely means of...
+                            <br> <a href="">[View More]</a>
+                        </P>
+                    </div>
+                </div>
+                <div class="col gradient_container_shwcase">
+                    <div class="gradient_container_skyblue">
+                        <h5 class="integrate">INTEGRATE</h5>
+                        <P>action on land degradation for identified landscapes with the Rio conventions and other multilateral environmental agreements and global initiatives...
+                        <br> <a href="">[View More]</a>
+                        </P>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--Pillars Section End-->
+    <!--Expert Section Start-->
+    <Section class="expert_section common_spacing">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col ">
+                    <div class="first_box">
+                    <h5>Expert Panel</h5>
+                    <p>
+                        The nominated national nodal/experts/organizations shall constitute an expert panel to the GIR and shall meet periodically to plan, discuss, and report progress on the voluntary ambitions of the GIR.
+                    </p>
+                    <p>
+                        Gandhinagar Information Platform – An enabler for implementation of the GIR Overall Objective of the Gandhinagar Information Platform
+                    </p>
+                    <p> 
+                        The platform shall display progress on the voluntary ambitions of the GIR and act as an information sharing hub for policies, best practices and actions. The platform will also foster collaboration between the nodal institutions/experts/organization while showcasing traditional and indigenous knowledge on ecological/ecosystem restoration of the identified landscapes and adopting a solutions approach to enable affordable, replicable and flexible solutions for the improvement of global restoration efforts.
+                    </p>
+                </div>
+                </div>
+                <div class="col initiative_box">
+                    <div class="secnd_box">
+                    <h5>Initiative Scope and Activities</h5>
+                    <p>
+                        The GIP will enable the operationalization of the GIR in the following ways –      
+                    </p>
+                    <p>
+                        <span>Engage stakeholders –</span> The Indian Council of Forestry Research & Education (ICFRE) and Indian Institute of 
+                        Forest Management (IIFM), will coordinate with nodal institutions/experts/organizations for the collation 
+                        of data, best practices and information received from participating countries as well as be responsible for
+                         the design, development and maintenance of the GIP. </p>
+                    <p> 
+                        <span>Enable collaboration on a voluntary basis – </span> The best practices, policies and success stories shared on the GIP 
+                        shall enable cross-learning as well as for ecological/ecosystem restoration.  </p>
+                        <p> 
+                            <span>Innovation through traditions –</span> Highlight traditional knowledge and role of other partners and 
+                                stakeholders (e.g., Indigenous Peoples, Local Communities and others as appropriate) for the 
+                                ecological/ecosystem restoration. </p>
+                                <p> 
+                                    <span>Access to and sharing of information</span> across a network of experts, researchers and civil 
+                                        society organizations invited by participating countries for capacity building activities. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <!--Expert Section End-->
+    <!--logo_section Start-->
+    <section class="logo_Section ">
+    <div class="container_fluid common_spacing">
+        <div class="flex_row">
+            <div class="col_12 gradient_container">
+                <h5>Nodal institutions</h5>
+                <p>
+                    Centre for Excellence under the Indian Council of Forestry Research and Education (ICFRE) is proposed to have the 
+                    responsibility as UNCCD regional hub for design, implementation, and maintenance of GIP for the priority landscape- 
+                    mining affected areas. Indian Institute of Forest Management (IIFM) will be the other regional hub for coordinating, 
+                    networking, and articulating inputs for restoration of forest fire affected areas. The two regional institutes 
+                    (viz. ICFRE and IIFM) shall act as the focal point for coordinating with the nodal institutes as nominated by the 
+                    G20 member countries to collect, collate, and identify suitable technologies which shall be low-cost, regionally
+                    conducive, and easily adoptable and replicable globally.
+                </p>
+            </div>
+        </div>
+        <div class="container">
+        <div class="flex_row">
+            <div class="col_3"></div>
+            <div class="col_2">
+                <img src="assets/images/aboutpage/icfre_logo.png">
+            </div>
+            <div class="col_2">
+                <img src="assets/images/aboutpage/Indian_Institute_of_Forest_Management.png">
+            </div>
+            <div class="col_2">
+                <img src="assets/images/aboutpage/image9.png">
+            </div>
+            <div class="col_3"></div>
+        </div>
+    </div>
+    </div>
+</section>
+    <!--logo_section End-->
+    <!--last Section Start-->
+    <section class="more_about_gip common_spacing">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_12">
+                <h2>To Know More About GIP</h2>
+            </div>
+            </div>
+            <div class="flex_row common_spacing">
+                <div class="col_2"></div>
+                <div class="col_3">
+                    <div class="delhi_declaration">
+                        <h4>Delhi Declaration</h4>
+                    </div>
+                </div>
+                <div class="col_3">
+                    <div class="grih_structure">
+                        <h4>GRIH Structure</h4>
+                    </div>
+                </div>
+                <div class="col_3">
+                    <div class="gir_gip_structure">
+                        <h4>GIR-GIP Alliance</h4>
+                    </div>
+                </div>
+                <div class="col_1"></div>
+            </div>
+        </div>
+
+    </section>
+    <!--last Section End-->
+    <!--last Section End-->
+    <?php include('includes/footer.php')?>
