38a92b23f9264a3adaf3ad26a55a35c94ab85fd4
abhisharma8860855650 <146344486+abhisharma8860855650@users.noreply.github.com>
2023-10-04 19:54:08 +0530

    Delhi declaration page and structure pagewith aboutpage webp image updated

---
+++ about_gip_structure.php
@@ -0,0 +1,175 @@
+<?php include('includes/header.php')?>
+     <!--Breadcrumb section Start-->
+     <Section class="breadcrumb ">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_9">
+                    <h2>Structure</h2>
+                </div>  
+                <div class="col_3 breadcrumb_lnk">
+                    <ul>
+                        <li>Home</li>
+                        <li>About GIP</li>
+                        <li>Structure</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <!--Breadcrumb section End-->
+    <!--First Section Start-->
+    <section class="about_gip_section   gradient_container_topbar">
+        <div class="flex_row">
+            <div class="col_10">
+            <h2 class="delhi_declrtion">Global Restoration Information Hub under the G20 Global Land Initiative</h2>
+            </div>
+            <div class="col_2">
+                <div class="flex_row top_discription_box_delhi_declrtn"></div>
+            </div>
+        </div>
+        
+    <div class="container_fluid ">
+                
+        <div class="flex_row">
+            <div class="col_12">
+                <p>The Ministerial Communique of the G20 Environmental Ministers Meet in Riyadh in 2020 defined the operating 
+                    parameters of the G20 Global Initiative on Reducing Land Degradation and Enhancing Conservation of Terrestrial 
+                    Habitats. One of the key elements of the initiative was to establish a land conservation, sustainable management, 
+                    and restoration website. The website was envisaged to contain the following:
+                </p>
+                <p><i class="icon-right-arrow"></i>&nbsp;Information on degraded lands, national and international commitments on land conservation and restoration </p>
+                <p><i class="icon-right-arrow"></i>&nbsp;Best practices, and success stories on land restoration, </p>
+                <p><i class="icon-right-arrow"></i>&nbsp;Progress-made, results achieved, and lessons learned on conservation and restoration </p>
+                <h5>It was further stated that:</h5>
+                <p><i class="icon-right-arrow"></i>&nbsp;The website will compile this information from publicly available sources and</p>
+                <p><i class="icon-right-arrow"></i>&nbsp;The website will serve as an information-sharing hub for easier access to information on land degradation, 
+                    conservation, and sustainable management.
+                </p>
+                <p>Currently, this website, the Global Restoration Information Hub (GRIH), is in the design stage with discussions 
+                    ongoing with global databases to improve interoperability. The website is expected to report data collected by 
+                    UNCCD as a part of its reporting process (PRAIS 4) and other information reported to various institutions and
+                     databases on land restoration.
+                </p>   
+            </div>
+        </div>
+    </div>
+</section>
+    <!--First  Section End-->
+     <!--Second Section Start-->
+     <section class="minister_images_section">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_6">
+                    <img src="assets/images/aboutpage/g20leaders.jpg" class="minister_images_g20">
+                </div>
+                <div class="col_6">
+                    <img src="assets/images/aboutpage/united_nation.webp" class="minister_images_g20">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Second Section Start-->
+    <!--GIP Section Start-->
+    <section class="abt_gip_section_Second">
+        <div class="container_fluid">
+            <div class="container ">
+                <div class="gradient_container">
+                <div class="flex_row">
+                    <div class="col_12">
+                        <h5>Enhancing the Global Restoration Information Hub under the G20 Global Land Initiative</h5>
+                        <p>India’s presidency has identified Arresting Land Degradation, Accelerating Ecosystem Restoration and 
+                            Enriching Biodiversity as one of the key priorities for deliberation as a part of the Environment and 
+                            Climate Sustainability Working Group. The presidency has committed to a decisive, inclusive and 
+                            action-oriented approach to enable the achievement of the G20 voluntary ambition of reducing land 
+                            degradation by 50% with the following objective:
+                        </p>
+                    </div>
+                </div>
+                <div class="flex_row objective_boxes">
+                    <div class="col_6">
+                        <img src="assets/images/aboutpage/enhance.webp" class="minister_images_g20">
+                    </div>
+                    <div class="col_6">
+                        <h5>Enhance focus on ecological restoration of land</h5>
+                        <p>Full ecological restoration is the act of restoring a landscape to ensure the habitat is able to sustain its 
+                            keystone species. A landscape approach for the restoration and reclamation of degraded land will ensure 
+                            sustainable and equitable land use while strengthening measures to mitigate and adapt to climate change.
+                        </p>
+                        <h5>Accelerate action on identified priority landscapes</h5>
+                        <p>Two priority landscapes have been proposed for accelerating ecosystem restoration</p>
+                        <h5>Forest Fire impacted areas</h5>
+                        <h5>Mining-affected areas</h5>
+                    </div>
+                </div>
+            </div>
+            </div>   
+        </div>
+    </section>
+    <!--GIP Section Start-->
+
+    <!--Third Section Start-->
+    <section class="discription_structr_about_gip common_spacing">
+        <div class="container_fluid common_spacing gradient_container">
+            <div class="flex_row">
+                <div class="col_12">
+                    <h5>Gandhinagar Information Platform</h5>
+                    <p>The vision for the Gandhinagar Information Platform is to encourage increased cooperation between G20 countries
+                         on sharing best practices on landscape restoration, technology transfer and mobilizing finance for the priority
+                          landscapes of forest-fire-impacted areas and mining-affected areas. This platform will aim to integrate best 
+                          practices of citizen and community engagement including successful land restoration projects along with
+                           traditional knowledge and practices to prevent landscape degradation, accelerate ecosystem restoration
+                            and enrich biodiversity
+                    </p>
+                    <p>The Gandhinagar For anformation Platform (GIP) that is proposed will enhance the Global Restoration Information
+                         Hub (GRIH) under the G20 Global Land Initiative. The GIP will be specific to the priority landscapes selected
+                          under the Indian Presidency i.e., forest fire-impacted areas and mining-affected areas. The hub will broadly
+                           comprise and cover the following components:
+                     </p>
+                     <p>
+                     <i class="icon-right-arrow"></i>&nbsp; Best practices, and success stories on land restoration across the priority landscapes i.e. forest fire-affected
+                        areas and mining-affected areas, information on the nodal institutes/agencies and knowledge bases
+                    </p>
+                    <p>
+                    <i class="icon-right-arrow"></i>&nbsp;Progress achieved, and lessons learned on restoration efforts
+                     </p>
+                     <p>
+                     The following discusses the specific components that will be encompassed in the Gandhinagar Information Hub:
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Third Section End -->
+    <!--last Section Start-->
+    <section class="more_about_gip common_spacing">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_12">
+                <h2>To Know More About GIP</h2>
+            </div>
+            </div>
+            <div class="flex_row common_spacing">
+                <div class="col_2"></div>
+                <div class="col_3">
+                    <div class="delhi_declaration">
+                        <h4>History of GIP</h4>
+                    </div>
+                </div>
+                <div class="col_3">
+                    <div class="grih_structure">
+                        <h4>GRIH Structure</h4>
+                    </div>
+                </div>
+                <div class="col_3">
+                    <div class="gir_gip_structure">
+                        <h4>GIR-GIP Alliance</h4>
+                    </div>
+                </div>
+                <div class="col_1"></div>
+            </div>
+        </div>
+
+    </section>
+    <!--last Section End-->
+    <!--last Section End-->
+    <?php include('includes/footer.php')?>
---
+++ delhi_declaration.php
@@ -0,0 +1,123 @@
+<?php include('includes/header.php')?>
+     <!--Breadcrumb section Start-->
+     <Section class="breadcrumb ">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_9">
+                    <h2>Delhi Declaration</h2>
+                </div>
+                <div class="col_3 breadcrumb_lnk">
+                    <ul>
+                        <li>Home</li>
+                        <li>About GIP</li>
+                        <li>Delhi Declaration</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <!--Breadcrumb section End-->
+    <!--First Section Start-->
+    <section class="history_gip_section   gradient_container_topbar">
+        <div class="flex_row">
+            <div class="col_10">
+            <h2 class="delhi_declrtion">Conserving, Protecting, Sustainably Using and Restoring Ecosystems</h2>
+            </div>
+            <div class="col_2">
+                <div class="flex_row top_discription_box_delhi_declrtn"></div>
+            </div>
+        </div>
+        
+    <div class="container_fluid ">
+                
+        <div class="flex_row">
+            <div class="col_12">
+                <p>We emphasize the importance of healthy ecosystems in addressing climate change, biodiversity loss, desertification,
+                     drought, land degradation, pollution, food insecurity and water scarcity. We commit to restoring by 2030 at least 
+                     30% of all degraded ecosystems and scaling up efforts to achieve land degradation neutrality. To achieve this,
+                      we:
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+    <!--First  Section End-->
+     <!--Second Section Start-->
+     <section class="minister_images_section">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_6">
+                    <img src="assets/images/aboutpage/g20leaders.jpg" class="minister_images_g20">
+                </div>
+                <div class="col_6">
+                    <img src="assets/images/aboutpage/g20leadersmeeting.jpg" class="minister_images_g20">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Second Section Start-->
+    <!--Third Section Start-->
+    <section class="discription_delhi_declaration">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_12">
+                    <p><i class="icon-right-arrow"></i>&nbsp;Commit to the swift, full and effective implementation of the Kunming-Montreal Global Biodiversity Framework
+                         (GBF), and encourage others to do the same, and encourage actions to halt and reverse biodiversity loss by
+                          2030. We also call for enhanced financial resources from all sources. To this end, we welcome the recent 
+                          establishment of the Global Biodiversity Framework Fund within the Global Environment Facility (GEF).
+                    </p>
+                    <p><i class="icon-right-arrow"></i>&nbsp;Support the G20 ambition to reduce land degradation by 50% by 2040 on a voluntary basis, as committed under the 
+                        G20 Global Land Initiative (GLI) and note the discussions on the Gandhinagar Implementation Roadmap and the 
+                        Gandhinagar Information Platform.
+                    </p>
+                    <p><i class="icon-right-arrow"></i>&nbsp;Recognize that forests provide crucial ecosystem services, as well as for climate purposes acting as sinks, at 
+                        the global and local levels for the environment, climate and people. We will scale up efforts to protect, 
+                        conserve and sustainably manage forests and combat deforestation, in line with internationally agreed 
+                        timelines, highlighting the contributions of these actions for sustainable development and taking into account 
+                        the social and economic challenges of local communities and indigenous peoples. In the context of forests, we 
+                        will avoid discriminatory green economic policies, consistent with WTO rules and multilateral environmental 
+                        agreements. We are committed to mobilizing new and additional finance for forests from all sources, including 
+                        concessional and innovative financing, in particular for developing countries. We commit to prevention and 
+                        mitigation of wildfires and remediation of mining-degraded lands.
+                    </p>
+                    <p><i class="icon-right-arrow"></i>&nbsp;Call for enhancing global cooperation and sharing of best practices on water and welcome the deliberations at 
+                        the UN 2023 Water Conference and G20 Dialogue on Water.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Third Section End -->
+    <!--last Section Start-->
+    <section class="more_about_gip common_spacing">
+        <div class="container_fluid">
+            <div class="flex_row">
+                <div class="col_12">
+                <h2>To Know More About GIP</h2>
+            </div>
+            </div>
+            <div class="flex_row common_spacing">
+                <div class="col_2"></div>
+                <div class="col_3">
+                    <div class="delhi_declaration">
+                        <h4>History of GIP</h4>
+                    </div>
+                </div>
+                <div class="col_3">
+                    <div class="grih_structure">
+                        <h4>GRIH Structure</h4>
+                    </div>
+                </div>
+                <div class="col_3">
+                    <div class="gir_gip_structure">
+                        <h4>GIR-GIP Alliance</h4>
+                    </div>
+                </div>
+                <div class="col_1"></div>
+            </div>
+        </div>
+
+    </section>
+    <!--last Section End-->
+    <!--last Section End-->
+    <?php include('includes/footer.php')?>
