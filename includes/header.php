2ee7a681b3c473e0166c60ac3e4c7024c26c3843
Mohit Kharbanda <uideveloper.mohit@gmail.com>
2023-10-16 17:00:38 +0530

    Add version in css file

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@
         rel="stylesheet">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css"
         type="text/css" media="screen" /> -->
-    <link rel="stylesheet" href="assets/scss/main-style.min.css">
+    <link rel="stylesheet" href="assets/scss/main-style.min.css?v=1.1">
     <?php
         $url = 'http://localhost/gip-code'; 
         // $url = parse_url($url, PHP_URL_SCHEME).'://'.parse_url($url, PHP_URL_HOST); 
