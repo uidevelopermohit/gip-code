33c3b7c76fed27f9307ca2b12035bf294bc8854a
abhisharma8860855650 <146344486+abhisharma8860855650@users.noreply.github.com>
2023-10-16 16:23:51 +0530

    data added

---
+++ contact.php
@@ -0,0 +1,73 @@
+<?php include('includes/header.php')?>
+     <!--Inner Banner Start-->
+     <Section class="inner_banner ">
+        <div class="container_fluid ">
+            <div class="flex_row">
+                <div class="col_8">
+                    <h4>Contact Us</h4>
+                </div>
+               
+                <div class="col_4">
+                    <ul class="new_breadcrumb">
+                        <li><a href="<?php echo $base_url; ?>">Home</a></li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <!--Inner Banner End-->
+    <section class="events_section">
+        <div class="container_fluid gradient_container">
+            <div class="container">
+                <div class="flex_row">
+                    <div class="col_12 common_spacing">
+                        <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+                        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
+                        of Lorem Ipsum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex_row common_spacing">
+                <div class="col">
+                    <div class="about_cards about_cards_1">
+                        <p class="cntct_cntnt">Lorem ipsum</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="about_cards about_cards_1">
+                        <p class="cntct_cntnt">Lorem ipsum</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="about_cards about_cards_1">
+                        <p class="cntct_cntnt">Lorem ipsum</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex_row common_spacing">
+                <div class="col">
+                    <div class="about_cards about_cards_1">
+                        <p class="cntct_cntnt">Lorem ipsum</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="about_cards about_cards_1">
+                        <p class="cntct_cntnt">Lorem ipsum</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="about_cards about_cards_1">
+                        <p class="cntct_cntnt">Lorem ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include('includes/footer.php')?>
---
+++ news_and_events.php
@@ -0,0 +1,87 @@
+<?php include('includes/header.php')?>
+     <!--Inner Banner Start-->
+     <Section class="inner_banner ">
+        <div class="container_fluid ">
+            <div class="flex_row">
+                <div class="col_8">
+                    <h4>News & Events</h4>
+                </div>
+               
+                <div class="col_4">
+                    <ul class="new_breadcrumb">
+                        <li><a href="<?php echo $base_url; ?>">Home</a></li>
+                        <li>News & Events</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <!--Inner Banner End-->
+    <section class="events_section">
+        <div class="container_fluid gradient_container">
+            <div class="flex_row common_spacing">
+                <div class="col">
+                    <div class="events_card">
+                        <div class="events_card_image">
+                            <img src="assets/images/events/bharat_mandapam.webp" alt="">
+                        </div>
+                        <div class="events_card_content">
+                            <h5>New Delhi Leaders’ Declaration:(9th September 2023, New Delhi)</h5>
+                            <p>9th September 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                     <div class="events_card">
+                        <div class="events_card_image">
+                            <img src="assets/images/events/ECSWG1.webp" alt="">
+                        </div>
+                        <div class="events_card_content">
+                            <h5>1st ECSWG meet: Bengaluru</h5>
+                            <p>February 9 to 11, 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="events_card">
+                        <div class="events_card_image">
+                            <img src="assets/images/events/2nd_ECWSG.webp" alt="">
+                        </div>
+                        <div class="events_card_content">
+                            <h5>2nd ECSWG meet: Gandhinagar</h5>
+                            <p>March 27 to 29,  2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex_row common_spacing">
+                <div class="col_4">
+                    <div class="events_card">
+                        <div class="events_card_image">
+                            <img src="assets/images/events/3rd_EWCSG.webp" alt="">
+                        </div>
+                        <div class="events_card_content">
+                            <h5>3rd ECSWG meet: Mumbai</h5>
+                            <p>May 21 to 23, 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="events_card">
+                        <div class="events_card_image">
+                            <img src="assets/images/events/ECSWG_Chennai.webp" alt="">
+                        </div>
+                        <div class="events_card_content">
+                            <h5>4th ECSWG meet: Chennai</h5>
+                            <p>July 26 to 28, 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include('includes/footer.php')?>
